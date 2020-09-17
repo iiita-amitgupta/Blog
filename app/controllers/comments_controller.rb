59f4ad48025d2f7e303f064695df8946e899d7de
Amit Gupta <iiita.amitgupta@gmail.com>
2020-09-17 16:04:44 +0530

    8. Deleting Comments; Error free; Deleting Comments and Associated Objects; output: Same as Ruby Documentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,15 @@ class CommentsController < ApplicationController
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
      
-      private
+    private
         def comment_params
           params.require(:comment).permit(:commenter, :body)
         end
