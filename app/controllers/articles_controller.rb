87c2527a55876f851dcbeaf8211fc07c2d37810c
Amit Gupta <iiita.amitgupta@gmail.com>
2020-09-17 13:48:36 +0530

    Error free; 5.10 Adding some validation; output: Same as Ruby Documentation(we show error if condition don't get satisfied)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
